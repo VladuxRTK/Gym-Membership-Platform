0a439056ce06e5b3064f20f6f8ef07b863a90ea8
Jurca Daniel <jurca.dani@gmail.com>
2020-05-16 16:52:09 +0300

    GMP-20

--- src/main/java/AbstractTypes/GymUser.java
+++ src/main/java/AbstractTypes/GymUser.java
@@ -1,8 +1,9 @@
 package AbstractTypes;
 
 public class GymUser extends User{
-   public GymUser(String username)
-   {
-	   this.username = username;
-   }
+    public GymUser(String username,String password)
+    {
+        this.username = username;
+        this.password=password;
+    }
 }
