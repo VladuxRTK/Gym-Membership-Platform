283afe63533a2693a57ca6c6128946bee236e989
Jula Alexandru <alexandru.jula@cs.upt.ro>
2020-05-17 16:58:43 +0300

    GMP-13

--- src/main/java/MainApp/EditUserGroup.java
+++ src/main/java/MainApp/EditUserGroup.java
@@ -355,6 +355,7 @@ public class EditUserGroup extends  JFrame implements ActionListener {
                     Object row[] = new Object[1];
                     row[0]=newObj.get("username");
                     model.addRow(row);
+                    model2.removeRow(table2.getSelectedRow());
 
 
 
