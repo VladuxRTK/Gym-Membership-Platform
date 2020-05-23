2dd5ea1c582a395600cc69da91357208a9eab94a
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2020-05-23 15:02:32 +0300

    GMP-34

--- src/main/java/MainApp/RegisterPage.java
+++ src/main/java/MainApp/RegisterPage.java
@@ -1,6 +1,7 @@
 package MainApp;
 
 import AuxiliaryStuff.JSONReader;
+import Exceptions.PasswordMustMatch;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -24,8 +25,9 @@ import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.util.Base64;
 import java.util.Iterator;
+import Exceptions.UserAlreadyTaken;
 
-public class RegisterPage extends JFrame implements ActionListener{
+public class RegisterPage extends JFrame implements ActionListener {
 	private JTextField username;
 	private JPasswordField password;
 	private JLabel usernameLabel;
@@ -39,8 +41,8 @@ public class RegisterPage extends JFrame implements ActionListener{
 	private int group;
 	private JSONArray array;
 	private JSONParser parserI;
-	public RegisterPage()
-	{
+
+	public RegisterPage() {
 		getContentPane().setBackground(new Color(65, 105, 225));
 		getContentPane().setForeground(new Color(65, 105, 225));
 
@@ -52,15 +54,15 @@ public class RegisterPage extends JFrame implements ActionListener{
 		usernameLabel.setBounds(175, 100, 150, 25);
 		passwordLabel = new JLabel("Password:");
 		passwordLabel.setBounds(175, 150, 150, 25);
-		
+
 		repeatPasswordLabel = new JLabel("Repeat password:");
 		repeatPasswordLabel.setLocation(135, 200);
 		repeatPasswordLabel.setSize(109, 25);
-		
-		repeatPassword=new JPasswordField();
+
+		repeatPassword = new JPasswordField();
 		repeatPassword.setSize(150, 25);
 		repeatPassword.setLocation(250, 200);
-		
+
 		radioButton1 = new JRadioButton("trainer");
 		radioButton1.setBackground(new Color(65, 105, 225));
 		radioButton1.setBounds(225, 250, 100, 30);
@@ -71,46 +73,53 @@ public class RegisterPage extends JFrame implements ActionListener{
 		register.setLocation(275, 300);
 		register.setSize(100, 30);
 		register.addActionListener(this);
-		
-
-    	ButtonGroup bg = new ButtonGroup();
-    	bg.add(radioButton1);
-    	bg.add(radioButton2);
-    	//passwordLabel.setBounds(175,150,150,25);
-    	
-    	//radioButton1.setBounds(225,200,100,30);
-    	//radioButton2.setBounds(325,200,100,30);
-    	
-    	//register.setBounds(258,250,100,30);
-    	getContentPane().setLayout(null);
-    	
-    	getContentPane().add(radioButton1);
-    	getContentPane().add(radioButton2);
-    	getContentPane().add(username);
-    	getContentPane().add(password);
-    	getContentPane().add(usernameLabel);
-    	getContentPane().add(passwordLabel);
-    	getContentPane().add(repeatPasswordLabel);
-    	getContentPane().add(repeatPassword);
-    	getContentPane().add(register);
 
 
-    	
-    	
-    	
-    	
-    	
-		
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(radioButton1);
+		bg.add(radioButton2);
+		//passwordLabel.setBounds(175,150,150,25);
+
+		//radioButton1.setBounds(225,200,100,30);
+		//radioButton2.setBounds(325,200,100,30);
+
+		//register.setBounds(258,250,100,30);
+		getContentPane().setLayout(null);
+
+		getContentPane().add(radioButton1);
+		getContentPane().add(radioButton2);
+		getContentPane().add(username);
+		getContentPane().add(password);
+		getContentPane().add(usernameLabel);
+		getContentPane().add(passwordLabel);
+		getContentPane().add(repeatPasswordLabel);
+		getContentPane().add(repeatPassword);
+		getContentPane().add(register);
+
+
 		this.setTitle("Register Page");
-		
-		this.setSize(600,400);
+
+		this.setSize(600, 400);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		String  usernameField = username.getText();
+
+		try {
+			registerAccount(e);
+		} catch (UserAlreadyTaken userAlreadyTaken) {
+			userAlreadyTaken.printStackTrace();
+			JOptionPane.showMessageDialog(this, "Username already taken!");
+		} catch (PasswordMustMatch passwordMustMatch) {
+			passwordMustMatch.printStackTrace();
+			JOptionPane.showMessageDialog(this, "Password must match!");
+		}
+
+
+		/*String  usernameField = username.getText();
 		String passwordField = password.getText();
 		String encodedPassword = Base64.getEncoder().encodeToString((passwordField).getBytes());
 		String repeatPasswordField = repeatPassword.getText();
@@ -211,12 +220,12 @@ public class RegisterPage extends JFrame implements ActionListener{
 			invalidAlgorithmParameterException.printStackTrace();
 		} catch (InvalidKeyException invalidKeyException) {
 			invalidKeyException.printStackTrace();
-		}
+		}*/
 
 
 	}
 
-	private final void Encrypt(String passwordField) throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException, ShortBufferException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
+	/*private final void Encrypt(String passwordField) throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException, ShortBufferException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
 
 		JSONObject obj = new JSONObject();
 		char[] pw = password.toString().toCharArray();
@@ -233,30 +242,104 @@ public class RegisterPage extends JFrame implements ActionListener{
 		cipherText = new byte[cipher.getOutputSize(input.length)];
 		int ctLength = cipher.update(input,0,input.length,cipherText,0);
 		ctLength += cipher.doFinal(cipherText,ctLength);
-		pw.setText(new String(cipherText));*/
+		pw.setText(new String(cipherText));
 		obj.put("ePassword",pw);
 
 
 
-	}
-	public void countGroups()
-	{
+	}*/
+	public void countGroups() {
 		group = 0;
 		parserI = new JSONParser();
 		array = new JSONArray();
 
-		array=JSONReader.readJSON("src/main/java/Resources/users.json",parserI);
+		array = JSONReader.readJSON("src/main/java/Resources/users.json", parserI);
 
 		Iterator<JSONObject> it = array.iterator();
 		while (it.hasNext()) {
 			JSONObject obj = it.next();
 			System.out.println("Here");
-			if(obj.get("role").toString().equals("trainer"))
-			{
+			if (obj.get("role").toString().equals("trainer")) {
 				group++;
 			}
 		}
 		System.out.println(group);
 
+	}
+
+	public void registerAccount(ActionEvent e) throws UserAlreadyTaken,PasswordMustMatch {
+		String usernameField = username.getText();
+		String passwordField = password.getText();
+		String encodedPassword = Base64.getEncoder().encodeToString((passwordField).getBytes());
+		String repeatPasswordField = repeatPassword.getText();
+		JButton clicked = (JButton) e.getSource();
+		countGroups();
+		JSONParser parser = new JSONParser();
+		JSONArray jsonArray;
+		try (Reader reader = new FileReader("src/main/java/Resources/users.json")) {
+			jsonArray = (JSONArray) parser.parse(reader);
+			boolean isThere = false;
+			Iterator<JSONObject> it = jsonArray.iterator();
+			while (it.hasNext() && isThere != true) {
+				JSONObject obj = it.next();
+				String userName = obj.get("username").toString();
+				if (userName.contentEquals(usernameField))
+					isThere = true;
+			}
+
+			if (isThere) {
+				//JOptionPane.showMessageDialog(this,"Username already taken!");
+				throw new UserAlreadyTaken("Username already taken!");
+			} else if (usernameField != null && passwordField.equals(repeatPasswordField) && clicked == register && radioButton1.isSelected()) {
+
+
+				JSONObject obj = new JSONObject();
+				obj.put("username", usernameField);
+				obj.put("password", encodedPassword);
+				obj.put("role", "trainer");
+				obj.put("group", Integer.toString(group + 1));
+				jsonArray.add(obj);
+
+
+				try (FileWriter file = new FileWriter("src/main/java/Resources/users.json")) {
+					file.write(jsonArray.toJSONString());
+					file.flush();
+
+					dispose();
+				} catch (IOException h) {
+					h.printStackTrace();
+				}
+			} else if (usernameField != null && passwordField.equals(repeatPasswordField) && clicked == register && radioButton2.isSelected()) {
+				//Encrypt(passwordField);
+
+				JSONObject obj = new JSONObject();
+				obj.put("username", usernameField);
+				obj.put("password", encodedPassword);
+				obj.put("role", "gymUser");
+				obj.put("membershipType", null);
+				obj.put("group", "unassigned");
+				jsonArray.add(obj);
+
+
+				try (FileWriter file = new FileWriter("src/main/java/Resources/users.json")) {
+					file.write(jsonArray.toJSONString());
+					file.flush();
+
+					dispose();
+				} catch (IOException h) {
+					h.printStackTrace();
+				}
+
+			} else if (usernameField != null && !passwordField.equals(repeatPasswordField) && clicked == register && (radioButton2.isSelected() || radioButton1.isSelected())) {
+				//JOptionPane.showMessageDialog(this, "Password must match!");
+				throw new PasswordMustMatch();
+			}
+
+
+		} catch (IOException h) {
+			h.printStackTrace();
+		} catch (ParseException h) {
+			h.printStackTrace();
+		}
 	}
 }
