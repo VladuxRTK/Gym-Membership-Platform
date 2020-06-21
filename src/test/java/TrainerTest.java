17ae9704ebd2a797c17bcb194ef754327feba581
VladuxRTK <33584762+VladuxRTK@users.noreply.github.com>
2020-06-21 03:35:00 +0300

    Test

--- src/test/java/TrainerTest.java
+++ src/test/java/TrainerTest.java
@@ -1,2 +1,16 @@
+import AbstractTypes.Trainer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TrainerTest {
+    private Trainer trainer;
+    @Test
+    public void getSetGroup()
+    {
+        trainer = new Trainer("abc","1");
+        trainer.setGroup("2");
+        assertEquals("2",trainer.getGroup());
+
+    }
 }
